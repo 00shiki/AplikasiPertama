2ec6bdf1e9de5af3f72ba5562be0973476b1a03d
Ahmad Luhur Pakerti <ahmadluhurp@gmail.com>
2024-03-25 20:55:47 +0700

    code convenience

--- app/src/main/java/com/example/aplikasipertama/ListAdapter.kt
+++ app/src/main/java/com/example/aplikasipertama/ListAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ListAdapter(val students: List<Student>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+class ListAdapter(private val students: List<Student>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val name: TextView = itemView.findViewById(R.id.tv_item_name)
         val major: TextView = itemView.findViewById(R.id.tv_item_major)
